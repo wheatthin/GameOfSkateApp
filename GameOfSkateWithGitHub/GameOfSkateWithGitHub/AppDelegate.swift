a8c9b333291b669f962ffee466efec2147221d29
Ethan <essanchez02@gmail.com>
2018-10-04 20:06:10 -0400

    gear

---
+++ GameOfSkateWithGitHub/GameOfSkateWithGitHub/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GameOfSkateWithGitHub
+//
+//  Created by Ethan Sanchez on 10/4/18.
+//  Copyright © 2018 Ethan Sanchez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GameOfSkateWithGitHub/GameOfSkateWithGitHub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GameOfSkateWithGitHub
+//
+//  Created by Ethan Sanchez on 10/4/18.
+//  Copyright © 2018 Ethan Sanchez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
